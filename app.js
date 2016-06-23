e2f93270456a8c753d78c7cfed98c99dcc532103
user <arjunair86@gmail.com>
2016-06-23 14:30:27 +0530

    Tested routing

--- app.js
+++ app.js
@@ -1,44 +1,65 @@
 var express = require ('express');
 var path = require('path');
 var fs = require ('fs');
+var bodyParser = require('body-parser');
 
+
+var router = express.Router();
 
 var app = express();
 var data = '';
 
+app.use(bodyParser());
+
+
+router.route('/')
+
+		.get(function(req, res){
+		  res.sendFile(path.join(__dirname + '/route.html'));
+		})
+		.post(function(req, res){
+		  res.send("Requested user: " + req.body.user);
+		});
+
+router.post('/inside_route', function(req, res){
+  res.send("Requested user: " + req.body.user);
+});
+
+
+app.use('/route', router);
 
 app.use(express.static(__dirname + '/View'));
 
 app.get('/', function(req, res){
-	res.sendFile('/index.html');
+  res.sendFile('/index.html');
 });
 
 app.get('/about', function(req, res){
-	res.sendFile(path.join(__dirname + '/about.html'));
+  res.sendFile(path.join(__dirname + '/about.html'));
 });
 
 var readStream = fs.createReadStream('test.txt', {highWaterMark: 0.5 * 1024});
 
 // readStream.on('data', function(chunk){
-// 	data += chunk;
+//  data += chunk;
 // });
 
 // readStream.on('end', function(){
-// 	console.log(data.toString());
+//  console.log(data.toString());
 // })
 
 var writeStream = fs.createWriteStream('output.txt');
 
 // readStream.on('data', function(chunk){
-// 	writeStream.write(chunk);
+//  writeStream.write(chunk);
 // })
 
 readStream.pipe(writeStream);
 
 
 app.get('/html', function(req, res){
-	res.sendFile(path.join(__dirname + '/output.txt'));
-	res.header('Content-Type', 'text/html');
+  res.sendFile(path.join(__dirname + '/output.txt'));
+  res.header('Content-Type', 'text/html');
 });
 
 
